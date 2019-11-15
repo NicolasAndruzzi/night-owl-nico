c4c8757aa9889ef8c3f0fc235fba6724384d4b4b
Nicolas Andruzzi <NicolasAndruzzi@gmail.com>
2019-11-15 09:58:06 -0700

    installed favico.js and implemented favicon video based on html5 video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,21 @@
-import { DOCUMENT } from '@angular/common';
-import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
-import { takeWhile } from 'rxjs/operators';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  title = 'night-owl-nico';
-  faviconLinkOverrideElelment: any = null;
-  numbers: any = interval(500);
-  regularInterval = this.numbers.pipe(takeWhile(() => this.alive));
-  currentFrame = 1;
-  totalNumberOfFrames = 240;
-  alive = true;
+export class AppComponent implements AfterViewInit {
+  // =============================================================
+  @ViewChild('faviconVideoElementNode', { static: false })
+  faviconVideoElementNode: ElementRef;
+  // =============================================================
+  Favico = require('favico.js');
+  favicon = new this.Favico();
 
-  constructor(@Inject(DOCUMENT) private document: Document) {
-    this.faviconLinkOverrideElelment = this.document.querySelector(
-      '#faviconLinkOverrideElelment'
-    );
-  }
-
-  ngOnInit() {
-    if (this.faviconLinkOverrideElelment) {
-      this.regularInterval.subscribe(() => this.rotateMoonFavicon());
+  ngAfterViewInit() {
+    if (this.faviconVideoElementNode) {
+      this.favicon.video(this.faviconVideoElementNode.nativeElement);
     }
-  }
-
-  rotateMoonFavicon(): void {
-    // console.time();
-    // tslint:disable-next-line: max-line-length
-    this.faviconLinkOverrideElelment.href = `/assets/simple-192x192-moon-rotation-frames/simple-192x192-moon-rotation-frame-${this.currentFrame}.png`;
-    this.currentFrame++;
-    this.currentFrame =
-      this.currentFrame > this.totalNumberOfFrames ? 1 : this.currentFrame;
-    // console.timeEnd();
-  }
-
-  ngOnDestroy() {
-    this.alive = false;
   }
 }
