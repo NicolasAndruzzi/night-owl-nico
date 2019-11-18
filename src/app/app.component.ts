ccc8cef488510bc886b91b5a98594d69a6e75b1f
Nicolas Andruzzi <NicolasAndruzzi@gmail.com>
2019-11-18 13:56:15 -0700

    moved video to hero component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,8 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import Favico from 'favico.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
-  // =============================================================
-  @ViewChild('faviconVideoElementNode', { static: false })
-  faviconVideoElementNode: ElementRef;
-  // =============================================================
-  favicon = new Favico();
-
-  ngAfterViewInit() {
-    if (this.faviconVideoElementNode) {
-      this.favicon.video(this.faviconVideoElementNode.nativeElement);
-    }
-  }
-}
+export class AppComponent {}
--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,12 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild
+} from '@angular/core';
+import Favico from 'favico.js';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.scss']
 })
-export class HeroComponent implements OnInit {
+export class HeroComponent implements OnInit, AfterViewInit {
   constructor() {}
+  // =============================================================
+  @ViewChild('faviconVideoElementNode', { static: false })
+  faviconVideoElementNode: ElementRef;
+  // =============================================================
+  favicon = new Favico();
+
+  ngAfterViewInit() {
+    if (this.faviconVideoElementNode) {
+      this.favicon.video(this.faviconVideoElementNode.nativeElement);
+    }
+  }
 
   ngOnInit() {}
 }
