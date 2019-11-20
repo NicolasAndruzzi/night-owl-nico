0f01eb5552559b575ef7ae9e3fa27fd04d39889a
Nicolas Andruzzi <NicolasAndruzzi@gmail.com>
2019-11-19 23:19:57 -0700

    moved video to header for sticky hero to header effect

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild
+} from '@angular/core';
+import Favico from 'favico.js';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
   constructor() {}
+  // =============================================================
+  @ViewChild('faviconVideoElementNode', { static: false })
+  faviconVideoElementNode: ElementRef;
+  // =============================================================
+  favicon = new Favico();
+
+  ngAfterViewInit() {
+    if (this.faviconVideoElementNode) {
+      this.favicon.video(this.faviconVideoElementNode.nativeElement);
+    }
+  }
 
   ngOnInit() {}
 }
--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,30 +1,12 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  OnInit,
-  ViewChild
-} from '@angular/core';
-import Favico from 'favico.js';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.scss']
 })
-export class HeroComponent implements OnInit, AfterViewInit {
+export class HeroComponent implements OnInit {
   constructor() {}
-  // =============================================================
-  @ViewChild('faviconVideoElementNode', { static: false })
-  faviconVideoElementNode: ElementRef;
-  // =============================================================
-  favicon = new Favico();
-
-  ngAfterViewInit() {
-    if (this.faviconVideoElementNode) {
-      this.favicon.video(this.faviconVideoElementNode.nativeElement);
-    }
-  }
 
   ngOnInit() {}
 }
