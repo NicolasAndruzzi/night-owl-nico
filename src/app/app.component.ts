93fd8948a4697a8fe5bdb80f97e94365bd5ce14b
Nicolas Andruzzi <NicolasAndruzzi@gmail.com>
2019-12-03 16:08:01 -0700

    testing console logging scroll position

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  HostListener,
-  ViewChild
-} from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  // @HostListener('window:scroll', ['$event']) onScrollEvent($event) {
+  //   console.log($event.target.scrollingElement.scrollTop);
+  // }
+}
