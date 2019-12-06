31634d1cb3fe7aeab63c7db8e7e5999b187213dc
Nicolas Andruzzi <NicolasAndruzzi@gmail.com>
2019-12-05 17:09:47 -0700

    added Night Owl Nico console tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   // @HostListener('window:scroll', ['$event']) onScrollEvent($event) {
   //   console.log($event.target.scrollingElement.scrollTop);
   // }
+
+  ngOnInit() {
+    console.log('---------------------------------------');
+    console.log('███╗   ██╗██╗ ██████╗ ██╗  ██╗████████╗');
+    console.log('████╗  ██║██║██╔════╝ ██║  ██║╚══██╔══╝');
+    console.log('██╔██╗ ██║██║██║  ███╗███████║   ██║');
+    console.log('██║╚██╗██║██║██║   ██║██╔══██║   ██║');
+    console.log('██║ ╚████║██║╚██████╔╝██║  ██║   ██║');
+    console.log('╚═╝  ╚═══╝╚═╝ ╚═════╝ ╚═╝  ╚═╝   ╚═╝');
+    console.log('');
+    console.log(' ██████╗ ██╗    ██╗██╗');
+    console.log('██╔═══██╗██║    ██║██║');
+    console.log('██║   ██║██║ █╗ ██║██║');
+    console.log('██║   ██║██║███╗██║██║');
+    console.log('╚██████╔╝╚███╔███╔╝███████╗');
+    console.log(' ╚═════╝  ╚══╝╚══╝ ╚══════╝');
+    console.log('');
+    console.log('███╗   ██╗██╗ ██████╗ ██████╗');
+    console.log('████╗  ██║██║██╔════╝██╔═══██╗');
+    console.log('██╔██╗ ██║██║██║     ██║   ██║');
+    console.log('██║╚██╗██║██║██║     ██║   ██║');
+    console.log('██║ ╚████║██║╚██████╗╚██████╔╝');
+    console.log('╚═╝  ╚═══╝╚═╝ ╚═════╝ ╚═════╝');
+    console.log('---------------------------------------');
+  }
 }
