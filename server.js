eb785586ec625ac39e89078e4a5c6ee281debcc3
Nicolas Andruzzi <NicolasAndruzzi@gmail.com>
2019-12-10 11:55:07 -0700

    updated code to enforce.HTTPS

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+var enforce = require('express-sslify');
 
 const app = express();
+
+// Use enforce.HTTPS({ trustProtoHeader: true }) in case you are behind
+// a load balancer (e.g. Heroku).
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 // Serve only the static files from the night-owl-nico directory
 app.use(express.static(__dirname + '/dist/night-owl-nico'));
